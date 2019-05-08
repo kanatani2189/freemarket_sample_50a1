0c6013ce1b33208e17e20fca6e4b05f6023d2025
yamamotokazutaka <owner@MA4-0014.local>
2019-05-08 13:29:10 +0900

    user_controllers

---
+++ app/controllers/user_profiles_controller.rb
@@ -0,0 +1,5 @@
+class UserProfilesController < ApplicationController
+  def edit
+    
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   root 'item_messages#index'
+  get "user_profiles" => "user_profiles#edit"
 end
