151f0ce760c64ff058aaee4b30756e2374a8689c
yamamotokazutaka <owner@MA4-0014.local>
2019-05-09 20:43:55 +0900

    deploy_basic

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,11 @@ class ApplicationController < ActionController::Base
 
   private
 
+  def production?
+    Rails.env.production?
+  end
+
+
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
       username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
