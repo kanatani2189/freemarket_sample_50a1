2551c1343f5f13fd3392ae2bf97a6d2860eb5f5b
yamamotokazutaka <owner@MA4-0014.local>
2019-05-09 20:18:25 +0900

    deploy_basic

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,3 +1,14 @@
+set :default_env, {
+  rbenv_root: "/usr/local/rbenv",
+  path: "/usr/local/rbenv/shims:/usr/local/rbenv/bin:$PATH",
+  AWS_ACCESS_KEY_ID: ENV["AWS_ACCESS_KEY_ID"],
+  AWS_SECRET_ACCESS_KEY: ENV["AWS_SECRET_ACCESS_KEY"],
+  BASIC_AUTH_USER: ENV["BASIC_AUTH_USER"],
+  BASIC_AUTH_PASSWORD: ENV["BASIC_AUTH_PASSWORD"]
+}
+
+set :linked_files, %w{ config/secrets.yml }
+
 # config valid for current version and patch releases of Capistrano
 lock "~> 3.11.0"
 
@@ -20,6 +31,18 @@ namespace :deploy do
   task :restart do
     invoke 'unicorn:restart'
   end
+
+  desc 'upload secrets.yml'
+  task :upload do
+    on roles(:app) do |host|
+      if test "[ ! -d #{shared_path}/config ]"
+        execute "mkdir -p #{shared_path}/config"
+      end
+      upload!('config/secrets.yml', "#{shared_path}/config/secrets.yml")
+    end
+  end
+  before :starting, 'deploy:upload'
+  after :finishing, 'deploy:cleanup'
 end
 
 # Default branch is :master
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,8 @@
 server '13.112.45.101', user: 'ec2-user', roles: %w{app db web}
+
+set :rails_env, "production"
+set :unicorn_rack_env, "production"
+
 # server-based syntax
 # ======================
 # Defines a single server with a list of roles and multiple properties.
