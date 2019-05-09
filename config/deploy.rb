984ddaa72c6c5356757bc0eefb9ab8e14c36d0fb
yamamotokazutaka <owner@MA4-0014.local>
2019-05-09 23:37:39 +0900

    Deploy_basic3

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,16 +1,8 @@
-set :default_env, {
-  rbenv_root: "/usr/local/rbenv",
-  path: "/usr/local/rbenv/shims:/usr/local/rbenv/bin:$PATH",
-  AWS_ACCESS_KEY_ID: ENV["AWS_ACCESS_KEY_ID"],
-  AWS_SECRET_ACCESS_KEY: ENV["AWS_SECRET_ACCESS_KEY"],
-   BASIC_AUTH_USER: ENV["BASIC_AUTH_USER"],
-   BASIC_AUTH_PASSWORD: ENV["BASIC_AUTH_PASSWORD"]
-}
+lock "~> 3.11.0"
 
-set :linked_files, %w{ config/secrets.yml }
 
 # config valid for current version and patch releases of Capistrano
-lock "~> 3.11.0"
+
 
 set :application, "freemarket_sample_50a1"
 set :repo_url, "git@github.com:kanatani2189/freemarket_sample_50a1.git"
@@ -25,6 +17,17 @@ set :ssh_options, auth_methods: ['publickey'],
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
 set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
 set :keep_releases, 5
+
+set :default_env, {
+  rbenv_root: "/usr/local/rbenv",
+  path: "/usr/local/rbenv/shims:/usr/local/rbenv/bin:$PATH",
+  # BASIC_AUTH_USER: ENV["BASIC_AUTH_USER"],
+  # BASIC_AUTH_PASSWORD: ENV["BASIC_AUTH_PASSWORD"],
+  AWS_ACCESS_KEY_ID: ENV["AWS_ACCESS_KEY_ID"],
+  AWS_SECRET_ACCESS_KEY: ENV["AWS_SECRET_ACCESS_KEY"]
+}
+
+set :linked_files, %w{ config/secrets.yml }
 
 after 'deploy:publishing', 'deploy:restart'
 namespace :deploy do
