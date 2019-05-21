7826c828a258c98200e61fddc2232a4992a4d5f8
nishidakousuke <nishidahikariyuu@nishidahikariyuunoMacBook-Air.local>
2019-05-21 14:39:38 +0900

    add-text_item.rb

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,19 @@
 class Item < ApplicationRecord
   has_many :user_items
   has_many :users, through: :user_items
-  has_many :images
-  accepts_nested_attributes_for :images
+  has_many_attached :images
+  has_many :parent_categories
+  accepts_nested_attributes_for :parent_categories
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :brand
+  belongs_to_active_hash :prefecture
+
+  validates :name, presence: true, length: { maximum: 40}
+  validates :description, presence: true
+  validates :condition, presence: true
+  validates :shipping_burden, presence: true
+  validates :shipping_method, presence: true
+  validates :prefecture_id, presence: true
+  validates :estimated_date, presence: true
+  validates :price, presence: true, :numericality => { :greater_than_or_equal_to => 300, :less_than_or_equal_to => 99999999 }
 end
