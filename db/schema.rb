4297efbf7b344886d5101bc051d22397946158ec
ucchiii <shop.rouro@gmail.com>
2019-05-21 19:04:56 +0900

    Edit-schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2019_05_14_075917) do
+ActiveRecord::Schema.define(version: 2019_05_15_093824) do
 
   create_table "cards", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "user_id", null: false
@@ -19,8 +18,7 @@ ActiveRecord::Schema.define(version: 2019_05_14_075917) do
     t.string "card_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    
-ActiveRecord::Schema.define(version: 2019_05_15_093824) do
+  end
 
   create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.text "comment"
