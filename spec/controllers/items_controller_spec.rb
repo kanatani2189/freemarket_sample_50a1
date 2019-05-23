f47b0538134b72adaf3b1b5e5d8d066921013cb6
nishidakousuke <nishidahikariyuu@nishidahikariyuunoMacBook-Air.local>
2019-05-23 15:26:38 +0900

    add_test-code

--- spec/controllers/items_controller_spec.rb
+++ spec/controllers/items_controller_spec.rb
@@ -5,13 +5,26 @@ describe ItemsController do
     it "renders the :index template" do
     end
   end
-end
 
-describe ItemsController do
   describe "GET #show" do
     it "renders the :show template" do
       get :show, params:{ id: 1}
       expect(response).to render_template :show
     end
   end
-end
+
+  describe 'GET #edit' do
+    it "assigns the requested tweet to @item" do
+      item = create(:item)
+      get :edit, params: {id: item}
+      expect(assigns(:item)).to eq item
+    end
+
+    it "renders the :edit template" do
+      item = create(:item)
+      get :edit, params: { id: item }
+      expect(response).to render_template :edit
+    end
+    end
+  end
+
