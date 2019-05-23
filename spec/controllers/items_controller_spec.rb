2a6e06fe73952e81dd22436f301cb154604c5a2f
nishidakousuke <nishidahikariyuu@nishidahikariyuunoMacBook-Air.local>
2019-05-23 16:12:16 +0900

    Change

--- spec/controllers/items_controller_spec.rb
+++ spec/controllers/items_controller_spec.rb
@@ -3,6 +3,8 @@ require 'rails_helper'
 describe ItemsController do
   describe 'GET #index' do
     it "renders the :index template" do
+      get :index
+      expect(response).to render_template :index
     end
   end
   
