5b93f739c59196d1230df2dedbaa52ef7b94de11
yamamotokazutaka <owner@MA4-0014.local>
2019-05-23 13:10:47 +0900

    Test_controller

--- spec/controllers/items_controller_spec.rb
+++ spec/controllers/items_controller_spec.rb
@@ -1,17 +1,22 @@
 require 'rails_helper'
 
-describe ItemsController do
-  describe 'GET #index' do
-    it "renders the :index template" do
-    end
-  end
-end
+# describe ItemsController do
+#   describe 'GET #index' do
+#     it "renders the :index template" do
+#     end
+#   end
+# end
 
 describe ItemsController do
   describe "GET #show" do
     it "renders the :show template" do
       get :show, params:{ id: 1}
       expect(response).to render_template :show
+    end
+  end
+
+  describe 'GET #index' do
+    it "renders the :index template" do
     end
   end
 end
