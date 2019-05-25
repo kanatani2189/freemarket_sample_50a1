41aed67233377add6d8aaffb4169ced3efbe0374
ishikawaakane <info@ishikawaakane.com>
2019-05-25 15:41:12 +0900

    Edit-routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,16 +3,15 @@ Rails.application.routes.draw do
   root 'items#index'
   resources :items, only: [:index, :new, :show, :create, :edit, :update]
   resources :users, only: [:index, :show, :new, :edit]
-  get "items/buy/123456789" =>"items#buy"
   # ログアウトページ（内田）
   get 'users/logout/123456789'       => 'users#logout'
 
   #pay.jp
   resources :cards, only: [:index, :new, :destroy] do
     collection do
-      post 'purchase'
-      get 'buy'
-      post 'pay'
+      post 'purchase' #pay.jp-POST
+      get 'buy' #購入確認画面
+      post 'pay' #pay.jp-POST
     end
   end
 end
