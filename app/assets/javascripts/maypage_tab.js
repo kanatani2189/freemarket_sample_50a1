d0546c7456761bbc5d8a37bdfd0ab1f70a7fd629
ucchiii <shop.rouro@gmail.com>
2019-05-28 20:52:31 +0900

    delete-console.log

--- app/assets/javascripts/maypage_tab.js
+++ app/assets/javascripts/maypage_tab.js
@@ -1,7 +1,6 @@
 $(function() {
   $('.items-show--container--content__tab li').click(function() {
     var index = $('.items-show--container--content__tab li').index(this);
-    console.log(index);
     $('.items-show--container--content__tab li').removeClass('active');
     $(this).addClass('active');
     $('.items-show--container--content__area ul').removeClass('show').eq(index).addClass('show');
