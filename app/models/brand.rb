83a3f08225894ab3995d6759a71a78e78b2df18d
yamamotokazutaka <owner@MA4-0014.local>
2019-05-29 16:22:28 +0900

    after_yamanisi_view2

--- app/models/brand.rb
+++ app/models/brand.rb
@@ -1,6 +1,6 @@
 class Brand< ActiveHash::Base
   self.data = [
     {id: 1, name: '---'}, 
-    {id: 2, name: 'シャネル'}, {id: 3, name: 'ナイキ'}
+    {id: 2, name: 'シャネル'}, {id: 3, name: 'ナイキ'},{id: 4, name: 'ルイ ヴィトン'}, {id: 5, name: 'シュプリーム'}
   ]
 end
